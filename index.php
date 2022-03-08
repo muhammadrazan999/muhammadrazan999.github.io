991e9de1fc1b3fc5a3d127ca76a9afb25c489d78
uchiha obito (Razan) <61842485+muhammadrazan999@users.noreply.github.com>
2022-03-08 23:31:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,283 @@
+<!doctype html>
+  <html>
+    <head>
+     <title> Razan Portofolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css">
+        <!-- Font Awesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- font google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css?family=Rancho&effect=anaglyph" rel="stylesheet">
+  <!-- efek font nama -->
+  <link rel="stylesheet" href="css/style1.css">
+  <!-- icon CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- animate CSS -->
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  <!-- sweetalert CSS -->
+  <link rel="stylesheet" href="css/sweetalert2.min.css">
+<!-- CSS owl -->
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+        <style>
+        *{
+          font-family: 'Shippori Antique B1', sans-serif;
+        }
+        body{
+          padding-bottom: 500px;
+        }
+        </style>
+     </head>
+     <body class="bg-dark">
+       <div class="container-fluid py-3 bg-dark font-effect-anaglyph mb-0 border border-info rounded-bottom d-inline-block" id="atas">
+         <h2 class="float-start text-center text-dark me-5">My Portofolio</h2>
+         <button class="navbar-toggler btn-outline-warning float-end ms-5 me-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <i class="fa-solid fa-bars"></i>
+    </button>
+       </div>
+        <div class="collapse navbar-collapse border-2 border-danger rounded font-effect-anaglyph" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 bg-warning border-2 border-danger">
+              <li class="nav-item ms-1"><a href="#guacuy" class="text-dark text-decoration-none"><font size="18px"><i class="fa-solid fa-house-user fa-5"></i></font><span class="ms-2">Home</span></a></li>
+              <hr>
+                 <li class="nav-item ms-1"><font size="18px"><a href="#myblog" class="text-dark text-decoration-none"><i class="fa-brands fa-blogger"></i></font><span class="ms-2">Blog</span></a></li>
+                   <hr>
+                    <li class="nav-item ms-1"><font size="18px"><a href="#hobigua" class="text-dark text-decoration-none"><i class="fa-solid fa-fire-flame-curved"></i></font><span class="ms-2">Hobi</span></a></li>
+                      <hr>
+                       <li class="nav-item ms-1 px-auto"><font size="18px"><a href="#pendidikan" class="text-dark text-decoration-none"><i class="fa-solid fa-graduation-cap"></i></font><span class="ms-2">Pendidikan</span></a></li>
+                         <hr>
+                          <li class="nav-item ms-1 px-auto"><font size="18px"><a href="#skill" class="text-dark text-decoration-none"><i class="fa-solid fa-chart-line"></i></font><span class="ms-2">Skills</span></a></li>
+              </ul>
+          </div>
+       <div class="container-fluid py-2 ahik-ahik bg-dark px-0" style="height:50vh;">
+       <img src="gambar/obi.png" class="rounded-circle border border-4 border-info mx-auto d-block mt-5 w-25">
+       <div class="container title mt-5 py-0 animate__jackInTheBox" >
+        <h6 class="mt-3 ">Muhammad Razan Rizqullah</h6>
+       </div>
+       <div class="d-flex-md mx-auto text-center px-0 mt-3 mb-5 border-2">
+           <button onclick='github()' class="btn btn-dark btn-outline-warning border-3 rounded-circle"><i class="fa-brands fa-github"></i></button>
+           <button class="btn btn-danger border-3 rounded-circle btn-outline-primary"><i class="fa-solid fa-play"></i></button>
+          </div>
+            <div class="container-fluid mt-3 px-0 py-0 mx-0 mb-0" style="height:40vh;">
+         <section class="mt-0 py-0">
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
+     </section>
+      <div class="container-fluid text-center mt-5 d-inline-block" id="guacuy">
+          <span class="badge bg-warning text-dark font-effect-anaglyph mt-0">About Me</span><br>
+            <button class="btn btn-info mt-2 rounded-pill" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"><i class="fa-solid fa-address-card"></i></button>
+
+<div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+  <div class="offcanvas-header font-effect-anaglyph">
+    <h5 class="offcanvas-title" id="offcanvasBottomLabel">Hai.. Kenalan yukk <i class="fa-solid fa-face-smile-beam"></i></h5> 
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body small font-effect-anaglyph bg-dark">
+    <p class="px-auto py-auto">
+      <img src="http://www.razancode.my.id/img/fotoku.jpg" class="img-thumbnail w-25 mb-2" alt="jelek banget ya aku" >
+      <br>
+   aku adalah <a href="" class="btn btn-sm btn-warning rounded">razan</a>, saya menyukai  <a href="" class="btn btn-sm btn-warning rounded">coding</a> khususnya  <a href="" class="btn btn-sm btn-warning rounded">pemrograman web</a>. Aku juga suka melakukan exploitasi Web salam kenal ya
+</p>
+  </div>
+</div>
+<!-- konten 2 -->
+
+<div class="row mt-5 py-2" id="myblog">
+   <div class="container d-block mt-2 mb-4">
+      <span class="badge bg-warning text-dark font-effect-anaglyph mt-0 mx-auto shadow-lg">My Blog/content</span>
+   </div>
+  <div class="col-sm-6 mt-3">
+    <div class="card  bg-warning rounded border-success border-3 font-effect-anaglyph shadow-lg">
+      <div class="card-body">
+          <font size="20px" color="#0098E4"><i class="fa-brands fa-wordpress"></i></font>
+        <br>
+        <h5 class="card-title">Wordpress Blog</h5>
+        <p class="card-text">pernah menulis di Wordpress Tutorial unfaedah</p>
+        <a href="#" class="btn btn-outline-secondary">preview</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 mt-3">
+    <div class="card bg-info rounded border-primary border-3 font-effect-anaglyph shadow-lg">
+      <div class="card-body">
+         <font size="20px" color="#CD612B"><i class="fa-brands fa-blogger"></i></font>
+        <br>
+        <h5 class="card-title">Blogger</h5>
+        <p class="card-text">pernah menulis di Blogger Tutorial unfaedah</p>
+        <a href="#" class="btn btn-outline-secondary">Preview</a>
+      </div>
+    </div>
+  </div>
+  
+   <div class="col-sm-6 mx-auto mt-3">
+    <div class="card rounded border-danger border-3 font-effect-anaglyph shadow-lg" style="background-color:#5A0D0E;">
+      <div class="card-body">
+      <font size="20px" color="#A72900"><i class="fa-brands fa-youtube"></i></font>
+        <br>
+        <h5 class="card-title">Youtube</h5>
+        <p class="card-text">Konten Kreator</p>
+        <a href="#" class="btn btn-outline-secondary">Preview</a>
+      </div>
+    </div>
+  </div>
+ <!-- <iframe width="640" height="360" scrolling="no" frameborder="0" style="border: none;" src="https://www.bitchute.com/embed/TTGc4U5oLkL8/"></iframe>-->
+</div>
+<!-- konten 3-->
+<div class="container-fluid px-auto py-1 mt-2" id="hobigua">
+   <span class="badge bg-warning text-dark font-effect-anaglyph mt-0">My Hobbies</span><br>
+<div class="owl-carousel owl-theme bg-dark mt-2 px-auto mx-auto">
+  <!-- hobby coding -->
+    <div class="item mx-auto px-auto">
+  <div class="card border-5 border-warning rounded-lg" style="width: 18rem;">
+  <img src="gambar/coding.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Coding</h5>
+    <p class="card-text">ini Hobi paling utama, karena coding itu asyik dia seperti teka teki silang.</p>
+  </div>
+</div>
+
+    </div>
+      <!-- hobby menggambar -->
+    <div class="item mx-auto px-auto">
+     <div class="card border-5 border-warning rounded-lg" style="width: 18rem;">
+  <img src="gambar/gambar.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Menggambar (manual/digital)</h5>
+    <p class="card-text">Menyukai Hobi ini sejak saya mengenal kartun jepang.</p>
+  </div>
+</div>
+      </div>
+      <!-- hobby catur -->
+    <div class="item mx-auto px-auto">
+      <div class="card border-5 border-warning rounded-lg" style="width: 18rem;">
+  <img src="gambar/catur2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Bermain Catur</h5>
+    <p class="card-text">Catur biasa saya lakukan ketika refresh daya berfikir terkadang saja.</p>
+  </div>
+</div>
+      
+      </div>
+      <!-- hobby sejarah -->
+    <div class="item mx-auto px-auto">
+       <div class="card border-5 border-warning rounded-lg" style="width: 18rem;">
+  <img src="gambar/war.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Membaca Sejarah</h5>
+    <p class="card-text">Saya Menyukai Sejarah yang bertopik peperangan dan revolusi.</p>
+  </div>
+      
+    </div>
+</div>
+</div>
+<!-- konten selanjutnya -->
+<div class="container-fluid px-auto py-1 mt-2" id="pendidikan">
+     <span class="badge bg-warning text-dark font-effect-anaglyph mt-0">Pendidikan</span><br>
+     <div class="mt-5 table-responsive font-effect-anaglyph">
+       <table class="table table-bordered table-bordered border-warning">
+  <thead class="table-warning border-warning">
+    <tr >
+      <th scope="col" colspan="2">Gambar</th>
+      <th scope="col">Almamater</th>
+      <th scope="col">Tahun Angkatan</th>
+      <th scope="col">Detail Info</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row" colspan="2"><img src="gambar/dosq.jpeg" class="img-thumbnail"></th>
+      <td>SMK MUHAMMADIYAH LEMAHABANG</td>
+      <td>2017-2020</td>
+     <td><button class="btn btn-info rounded-circle text-light"><i class="fa-solid fa-eye"></i></button></td>
+    </tr>
+    <tr>
+      <th scope="row" colspan="2"><img src="gambar/umb.jpeg" class="img-thumbnail rounded"></th>
+      <td>Universitas Muhammadiyah Bandung</td>
+      <td>2020 - Sekarang</td>
+      <td><button class="btn btn-info rounded-circle text-light"><i class="fa-solid fa-eye"></i></button></td>
+    </tr>
+  </tbody>
+</table>
+     </div>
+  
+  </div>
+  <!-- konten tentang skill -->
+  <div class="container-fluid mx-auto px-auto" id="skill">
+     <span class="badge bg-warning text-dark font-effect-anaglyph mt-0">Skills Pemrograman</span><br>
+     <div class="container mx-auto px-auto font-effect-anaglyph" style="justify-content:relative">
+       <canvas id="myChart" class="text-warning"></canvas>
+     </div>
+     
+  </div>
+  <!-- footer -->
+    <div class="fixed-bottom mb-5">
+     <a href="#atas" class="btn btn-info btn-lg rounded-circle mb-4 float-end me-2"><i class="fa-solid fa-up-long"></i></a>
+     </div>
+  <div class="fixed-bottom mt-5">
+  <nav class="navbar navbar-light fixed-bottom bg-dark mt-4">
+  <div class="container-fluid d-inline-block text-warning px-5">
+    <span class="navbar-text text-warning mx-3">
+      powered by uchiha obITo - Razan
+    </span>
+   <a href="" class="text-decoration-none text-warning"><i class="fa-brands fa-instagram"></i></a>
+    <a href="" class="text-decoration-none text-warning"><i class="fa-brands fa-whatsapp"></i></a>
+     <a href="" class="text-decoration-none text-warning"><i class="fa-brands fa-facebook"></i></a>
+  </div>
+</nav>
+</div>
+
+          </div>
+     </div>
+       </div>
+       <script src="js/jquery.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script type="text/javascript">
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    animateOut: 'animate__flipOutX',
+    animateIn: 'animate__flipOutY',
+    smartSpeed:450,
+    responsiveClass:false,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        }
+    }
+})
+</script>
+     </body>
+     <script>
+     function github(){
+Swal.fire({
+  title: '<strong>My Github Account</strong>',
+  html:
+    '<iframe src="https://gist.github.com/muhammadrazan999/633461ad645380ea466f0ea4eb8ee58f" width="680" height="480" allowfullscreen></iframe>',
+  footer:'<a href="https://github.com/muhammadrazan999" class="btn btn-info text-light">Go</a>'
+});
+}
+     </script>
+     <!-- sweetalert2 js -->
+     <script src="js/sweetalert2.all.min.js"></script>
+     <!-- bootstrap js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+  crossorigin="anonymous">
+  </script>
+  <!-- JS chart -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"
+  integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ==" crossorigin="anonymous" 
+  referrerpolicy="no-referrer">
+  </script>
+  <!-- setting JS chart -->
+  <script src="js/konfigurasiChart.js"></script>
+    </html>
